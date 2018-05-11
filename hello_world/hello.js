5a39329b70309de35b1a017833f31aae89509842
Marcelo Ceolin <fxsobr@gmail.com>
2018-05-11 19:58:54 -0300

    exemplo1
    adicionando exemplo 1 .

--- hello_world/hello.js
+++ hello_world/hello.js
@@ -1,1 +1,10 @@
 console.log("olá mundo");
+const fs = require("fs");
+const os = require("os");
+
+let user = os.userInfo();
+
+fs.appendFileSync("texto.txt", "nome do usuário\n");
+fs.appendFileSync("texto.txt", `Username : ${user.username}\n `);
+fs.appendFileSync("texto.txt", `Home Folder : ${user.homedir}\n \n `);
+console.log(user);
